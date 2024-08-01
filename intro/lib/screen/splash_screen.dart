cbf6a8b28963335ac2eeb7745466efb10432f33b
BORA <a52447879@gmail.com>
2024-08-01 16:23:56 +0900

    feat: SplashScreen, SectionText widget

--- intro/lib/screen/splash_screen.dart
+++ intro/lib/screen/splash_screen.dart
@@ -1,10 +1,13 @@
 // 앱 실행시 이동되는 곳
 
 import 'package:flutter/material.dart';
+import 'package:intro/widget/texts.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
+  // StatefulWidget을 상속하는 클래스는 반드시 createState() 메서드를 구현해야 합니다.
+  // 이 메서드는 상태를 관리하는 State 객체를 반환하는 데 사용됩니다
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
@@ -14,7 +17,10 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('시작화면 입니다.!!!'),
+        child: SectionText(
+          text: '이메일',
+          textColor: Color(0xff979797),
+        ),
       ),
     );
   }
