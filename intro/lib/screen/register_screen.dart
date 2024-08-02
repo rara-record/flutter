b8a57e030078691bf1e380adc657060e3d6a1724
BORA <a52447879@gmail.com>
2024-08-02 19:29:53 +0900

    feat: 회원가입 스크린에서 닉네임 텍스트필드 추가

--- intro/lib/screen/register_screen.dart
+++ intro/lib/screen/register_screen.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:intro/widget/appbar.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:intro/widget/text_fields.dart';
+import 'package:intro/widget/texts.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -13,6 +15,11 @@ class RegisterScreen extends StatefulWidget {
 
 class _RegisterScreenState extends State<RegisterScreen> {
   File? profileImg;
+  final TextEditingController _nameController = TextEditingController();
+  // final TextEditingController _emailController = TextEditingController();
+  // final TextEditingController _passwordController = TextEditingController();
+  // final TextEditingController _passwordReController = TextEditingController();
+  // final TextEditingController _introduceController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -21,15 +28,46 @@ class _RegisterScreenState extends State<RegisterScreen> {
         title: 'Food PICK 가입하기',
         isLeading: true,
       ),
-      body: Center(
+      body: Container(
+        margin: const EdgeInsets.all(16),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            // 프로필 사진
             GestureDetector(
                 child: _buildProfile(),
                 // 프로필 이미지 변경 및 삭체 팝업 띄우기
                 onTap: () {
                   showBottomSheetAboutProfile();
-                })
+                }),
+
+            const SizedBox(height: 16),
+
+            // 섹션 및 입력 필드들
+            const SectionText(text: '닉네임', textColor: Color(0xff979797)),
+            const SizedBox(height: 8),
+            TextFormFieldCustom(
+              hintText: '닉네임을 입력해주세요',
+              isPasswordField: false,
+              isReadOnly: false,
+              keyboardType: TextInputType.name,
+              textInputAction: TextInputAction.next,
+              validator: (value) => inputNameValidator(value),
+              controller: _nameController,
+            ),
+
+            const SizedBox(height: 16),
+
+            const SectionText(text: '이메일', textColor: Color(0xff979797)),
+
+            const SizedBox(height: 16),
+            const SectionText(text: '비밀번호', textColor: Color(0xff979797)),
+
+            const SizedBox(height: 16),
+            const SectionText(text: '비밀번호 확인', textColor: Color(0xff979797)),
+
+            const SizedBox(height: 16),
+            const SectionText(text: '자기소개', textColor: Color(0xff979797)),
           ],
         ),
       ),
@@ -86,7 +124,10 @@ class _RegisterScreenState extends State<RegisterScreen> {
               ),
               // 앨범에서 사진 선택
               TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pop(context);
+                  getGalleryImage();
+                },
                 child: const Text('앨범에서 사진 선택',
                     style: TextStyle(
                       color: Colors.black,
@@ -95,7 +136,10 @@ class _RegisterScreenState extends State<RegisterScreen> {
               ),
               // 프로필 사진 삭제
               TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pop(context);
+                  deleteProfileImage();
+                },
                 child: const Text('프로필 사진 삭제',
                     style: TextStyle(
                       color: Colors.black,
@@ -111,11 +155,41 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
   Future<void> getCameraImage() async {
     // 카메라로 사진 촬영하여 이미지 파일을 가져오는 함수
-    var image = await ImagePicker().pickImage(source: ImageSource.camera);
+    var image = await ImagePicker().pickImage(
+      source: ImageSource.camera,
+      imageQuality: 10,
+    );
     if (image != null) {
       setState(() {
         profileImg = File(image.path);
       });
+    }
+  }
+
+  Future<void> getGalleryImage() async {
+    // 앨범에서 사진 선택하는 함수
+    var image = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 10,
+    );
+    if (image != null) {
+      setState(() {
+        profileImg = File(image.path);
+      });
+    }
+  }
+
+  void deleteProfileImage() {
+    // 프로필 사진 삭제
+    setState(() {
+      profileImg = null;
+    });
+  }
+
+  inputNameValidator(value) {
+    // 닉네임 필드 검증 함수
+    if (value.isEmpty) {
+      return 'ㄴ닉네임을 입력해주세요';
     }
   }
 }
