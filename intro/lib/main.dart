640e82f5e5a584d53a42fe49890c3a39c5e2bf9f
BORA <a52447879@gmail.com>
2024-08-11 22:09:50 +0900

    init

--- intro/lib/main.dart
+++ intro/lib/main.dart
@@ -1,22 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:intro/pages/person_page.dart';
-import 'package:intro/widgets/custom_button.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-
-part 'main.g.dart';
 
 Future<void> main() async {
   // main 메소드에서 비동기로 데이터를 다루는 상황이 있을 때 반드시 최초에 호출해줘야 함
   WidgetsFlutterBinding.ensureInitialized();
-
-  // init supabase
-  await Supabase.initialize(
-    url: 'https://fnpeucnejzwsbcczaohr.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZucGV1Y25lanp3c2JjY3phb2hyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI0ODg1NTgsImV4cCI6MjAzODA2NDU1OH0.R0Cb8hYmpAwLzeB-F43qpfdTPaNZSTRlD_VxNVmPxHs',
-  );
 
   runApp(const ProviderScope(child: MyApp()));
 }
@@ -49,12 +36,7 @@ class MyHomePage extends StatelessWidget {
         child: ListView(
           shrinkWrap: true,
           padding: const EdgeInsets.all(20),
-          children: const [
-            CustomButton(
-              title: 'Person',
-              child: PersonPage(),
-            )
-          ],
+          children: const [],
         ),
       ),
     );
