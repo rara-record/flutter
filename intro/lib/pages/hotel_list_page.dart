460d95a17d1853f776c3f465926b3650e55c7624
BORA <a52447879@gmail.com>
2024-08-11 22:34:08 +0900

    feat: hotel page init

---
+++ intro/lib/pages/hotel_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HotelListPage extends StatefulWidget {
+  const HotelListPage({super.key});
+
+  @override
+  State<HotelListPage> createState() => _HotelListPageState();
+}
+
+class _HotelListPageState extends State<HotelListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
